9f3b12105a021b0ce0d19377ee77c4a0fe6df090
Subdij <ziyadou2011@hotmail.fr>
2025-01-19 04:15:34 +0100

    Enhance WiFi connection script to check if RBTray is running and add cleanup function on exit

--- run_wifi_connect.bat
+++ run_wifi_connect.bat
@@ -9,8 +9,14 @@ for /f "tokens=1,2 delims==" %%i in (C:\Users\ziyad\Desktop\wifi-connexion\.env)
 REM Charger le nom d'utilisateur à partir du fichier .env
 for /f "tokens=1,2 delims==" %%i in (C:\Users\ziyad\Desktop\wifi-connexion\.env) do if "%%i"=="USERNAME" set USERNAME=%%j
 
-REM Lancer RBTray pour permettre la minimisation dans la zone de notification
-start "" "%RBTRAY_PATH%"
+REM Vérifier si RBTray est déjà en cours d'exécution
+tasklist /FI "IMAGENAME eq RBTray.exe" 2>NUL | find /I /N "RBTray.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo RBTray est deja en cours d'execution.
+) else (
+    REM Lancer RBTray pour permettre la minimisation dans la zone de notification
+    start "" "%RBTRAY_PATH%"
+)
 
 :loop
 REM Lancer le script Python sans afficher le terminal
@@ -21,3 +27,11 @@ timeout /t 12600 /nobreak
 
 REM Recommencer la boucle
 goto loop
+
+:cleanup
+REM Fermer RBTray
+taskkill /IM RBTray.exe /F
+exit
+
+REM Appeler la fonction de nettoyage à la fermeture du terminal
+trap cleanup EXIT
